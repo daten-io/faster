7a1ae03868feb02964fc77ee942ea9f5962c296c
Armin <me@obrown.io>
2017-09-30 16:56:09 +0200

    improved Byte Array Comparison

---
+++ benchmarks/src/main/java/io/daten/faster/benchmarks/FasterByteComparisonBenchmark.java
@@ -0,0 +1,179 @@
+package io.daten.faster.benchmarks;
+
+import io.daten.faster.FasterByteComparison;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Warmup(iterations = 3, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+public class FasterByteComparisonBenchmark {
+
+    private static final int COMPARISONS_PER_INVOCATION = 10_000_000;
+
+    private static final byte[] MANY_BYTES;
+
+    private static final byte[] ONE_BYTE;
+
+    private static final byte[] TWO_BYTES;
+
+    private static final byte[] THREE_BYTES;
+
+    private static final byte[] FOUR_BYTES;
+
+    private static final byte[] FIVE_BYTES;
+
+    private static final byte[] SIX_BYTES;
+
+    private static final byte[] SEVEN_BYTES;
+
+    private static final byte[] EIGHT_BYTES;
+
+    private static final byte[] NINE_BYTES;
+
+    private static final byte[] TEN_BYTES;
+
+    private static final byte[] THIRTY_TWO_BYTES;
+
+    static {
+        final Random random = new Random();
+        MANY_BYTES = new byte[128];
+        random.nextBytes(MANY_BYTES);
+        ONE_BYTE = Arrays.copyOf(MANY_BYTES, 1);
+        TWO_BYTES = Arrays.copyOf(MANY_BYTES, 2);
+        THREE_BYTES = Arrays.copyOf(MANY_BYTES, 3);
+        FOUR_BYTES = Arrays.copyOf(MANY_BYTES, 4);
+        FIVE_BYTES = Arrays.copyOf(MANY_BYTES, 5);
+        SIX_BYTES = Arrays.copyOf(MANY_BYTES, 6);
+        SEVEN_BYTES = Arrays.copyOf(MANY_BYTES, 7);
+        EIGHT_BYTES = Arrays.copyOf(MANY_BYTES, 8);
+        NINE_BYTES = Arrays.copyOf(MANY_BYTES, 9);
+        TEN_BYTES = Arrays.copyOf(MANY_BYTES, 10);
+        THIRTY_TWO_BYTES = Arrays.copyOf(MANY_BYTES, 32);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareOneByte(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(ONE_BYTE, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareTwoBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(TWO_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareThreeBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(THREE_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareFourBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(FOUR_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareFiveBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(FIVE_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareSixBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(SIX_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareSevenBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(SEVEN_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareEightBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(EIGHT_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareNineBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(NINE_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareTenBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(TEN_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(COMPARISONS_PER_INVOCATION)
+    public final void compareThirtyTwoBytes(final Blackhole blackhole) {
+        int res = 0;
+        for (int i = 0; i < COMPARISONS_PER_INVOCATION; ++i) {
+            res += FasterByteComparison.compare(THIRTY_TWO_BYTES, MANY_BYTES);
+        }
+        blackhole.consume(res);
+    }
+}
--- core/src/main/java/io/daten/faster/FasterByteComparison.java
+++ core/src/main/java/io/daten/faster/FasterByteComparison.java
@@ -16,45 +16,23 @@ public final class FasterByteComparison {
 
     public static int compare(final byte[] buffer1, final int offset1, final int length1,
         final byte[] buffer2, final int offset2, final int length2) {
-        if (buffer1 == buffer2 &&
-            offset1 == offset2 &&
-            length1 == length2) {
-            return 0;
-        }
         final int minLength = Math.min(length1, length2);
-        final int minWords = minLength / Long.BYTES;
+        // Shortcut for (minLength / 8) * 8
+        final int fullWordBytes = minLength & ~7;
         final int offset1Adj = offset1 + UnsafeUtil.BYTE_ARRAY_OFFSET;
         final int offset2Adj = offset2 + UnsafeUtil.BYTE_ARRAY_OFFSET;
-        for (int i = 0; i < minWords * Long.BYTES; i += Long.BYTES) {
+        for (int i = 0; i < fullWordBytes; i += Long.BYTES) {
             final long left = UNSAFE.getLong(buffer1, offset1Adj + (long) i);
             final long right = UNSAFE.getLong(buffer2, offset2Adj + (long) i);
-            final long diff = left ^ right;
-            if (diff != 0L) {
-                if (!PlatformUtil.IS_LITTLE_ENDIAN) {
-                    return lessThanUnsigned(left, right) ? -1 : 1;
-                }
-                // Use binary search
-                int n = 0;
-                int y;
-                int x = (int) diff;
-                if (x == 0) {
-                    x = (int) (diff >>> 32);
-                    n = 32;
-                }
-                y = x << 16;
-                if (y == 0) {
-                    n += 16;
+            if (left != right) {
+                if (PlatformUtil.IS_LITTLE_ENDIAN) {
+                    return Long.compareUnsigned(Long.reverseBytes(left), Long.reverseBytes(right));
                 } else {
-                    x = y;
+                    return Long.compareUnsigned(left, right);
                 }
-                y = x << 8;
-                if (y == 0) {
-                    n += 8;
-                }
-                return (int) ((left >>> n & 0xFFL) - (right >>> n & 0xFFL));
             }
         }
-        for (int i = minWords * Long.BYTES; i < minLength; i++) {
+        for (int i = fullWordBytes; i < minLength; i++) {
             final int result = Long.compareUnsigned(
                 buffer1[offset1 + i],
                 buffer2[offset2 + i]
@@ -66,7 +44,4 @@ public final class FasterByteComparison {
         return length1 - length2;
     }
 
-    private static boolean lessThanUnsigned(final long x1, final long x2) {
-        return x1 + Long.MIN_VALUE < x2 + Long.MIN_VALUE;
-    }
 }
